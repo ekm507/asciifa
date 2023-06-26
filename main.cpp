5f9703a73112ef7c7a8b2df5f6de0f85dd5076a6
Erfan Kheyrollahi <ekm507@gmail.com>
2023-06-26 21:31:32 +0330

    read file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+    string font_filename = "./fonts/aipara.aff";
+    ifstream fontfile(font_filename);
+    string fontfile_line;
+    while (getline (fontfile, fontfile_line)) {
+    // Output the text from the file
+    cout << fontfile_line << endl;
+    }
 
     return 0;
 }
